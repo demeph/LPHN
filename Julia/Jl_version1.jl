3496ad8bac8226589e7ba78cfcec1d0b52ce569d
demeph <demetre.phalavandishvili@gmail.com>
2017-11-30 15:52:03 +0100

    version 1 du algo en julia

---
+++ Julia/Jl_version1.jl
@@ -0,0 +1,31 @@
+function afficherPremier(n)
+    return [ a for a in 1:n if lesbooleans[a] == true ]
+end
+
+function elimineMultiples(x,n)
+    for i in (x+1):n
+        if i%x == 0
+            lesbooleans[i] = false
+        end
+    end
+end
+
+function trouvePremier(n)
+    for i in 1:n
+        if i == 1
+            lesbooleans[i] = false
+        elseif lesbooleans[i] == true
+            elimineMultiples(i,n)
+        end
+    end
+end
+
+function prog_principale(n)
+    global lesbooleans = fill(true,n)
+    println(afficherPremier(n))
+    trouvePremier(n)
+    println("apres traitement")
+    println(afficherPremier(n))
+end
+
+prog_principale(100)
