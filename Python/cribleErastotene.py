ac7ddf68e4824c3f60f51803f7ce3abc251fe5df
demeph <demetre.phalavandishvili@gmail.com>
2017-12-04 17:50:35 +0100

    modif python

--- Python/cribleErastotene.py
+++ Python/cribleErastotene.py
@@ -1,14 +1,19 @@
 import time
 
 
-def eratosthenes2(n):
-    multiples = set()
-    for i in range(2, n + 1):
-        if i not in multiples :
-            yield i
-            multiples.update(range(i * i, n + 1, i))
+def trouvePremiers(n):
+    ensembles = set()
+    prim = set()
+    i = 2
+    while i < n :
+        if i not in ensembles :
+            ensembles.update(range(i * i, n + 1, i))
+            prim.add(i)
+        i = i+1
+    return  prim
 
 t_avant = time.time()
-print(list(eratosthenes2(15)))
+print(sorted(trouvePremiers(15)))
 t_apres = time.time()
-print("temps d'execution : ",t_apres-t_avant)
+print("temps d'execution : ",t_apres - t_avant)
+
