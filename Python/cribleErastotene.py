5b9c566fe0d21319b821b2702164ebf790be4764
loic44650 <loic44650@hotmail.fr>
2017-12-06 18:22:17 +0100

    add comment

--- Python/cribleErastotene.py
+++ Python/cribleErastotene.py
@@ -1,21 +1,19 @@
 # Fonction Crible d'Eratosthène en Python
 # @author Mahier Loïc & PHALAVANDISHVILI Demtre
 
-import time
-
 def trouvePremiers(n):
     ensembles = set()
     nbPremiers = set()
+    # on parcourt de 2 à n, on ignore 1 car on sait qu'il n'est pas premier
     i = 2
     while i < n :
+    	# on ajoute les nom-premiers dans ensembles
+    	# et les premiers dans nbPremiers
         if i not in ensembles :
             ensembles.update(range(i * i, n + 1, i))
             nbPremiers.add(i)
-        i = i+1
+        i = i + 1
     return nbPremiers
 
-t_avant = time.time()
+# on affiche le set
 print(sorted(trouvePremiers(15)))
-t_apres = time.time()
-print("temps d'execution : ",t_apres - t_avant)
-
